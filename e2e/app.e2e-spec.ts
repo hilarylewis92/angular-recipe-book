c3a63f5de0b3599da1817fd4dda0e310aa5151c1
Hilary Lewis <hilarylewis92@gmail.com>
2017-10-16 14:56:44 -0600

    Add recipe componet layout

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { RecipeBookPage } from './app.po';
 
 describe('recipe-book App', () => {
-  let page: RecipeBookPage;
+    let page: RecipeBookPage;
 
-  beforeEach(() => {
-    page = new RecipeBookPage();
-  });
+    beforeEach(() => {
+        page = new RecipeBookPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
-  });
+    it('should display welcome message', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    });
 });
